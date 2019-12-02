3842ab17c3706d2c12e1f05be3aba5bc11b08ddd
Fiby <flamefiby@icloud.com>
2019-12-02 14:36:28 -0500

    Add files via upload

---
+++ CPU.java
@@ -0,0 +1,7 @@
+package com.Phone.Part;
+
+public class CPU{
+    public String manufacturer;
+    public double version;
+    public int speed;
+}
---
+++ Memory.java
@@ -0,0 +1,8 @@
+package com.Phone.Part;
+
+public class Memory {
+    public String manufacturer;
+    public int memory;
+    public String memoryType;
+
+}
